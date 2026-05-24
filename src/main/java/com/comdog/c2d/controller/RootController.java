ecc9831dab90c0838a44cfc625f8a44c22bce1e0
hjkikyou <2507110310X@gmail.com>
2026-05-24 20:24:37 +0900

    feat: RootController 추가

---
+++ src/main/java/com/comdog/c2d/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.comdog.c2d.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+	
+	@GetMapping("/")
+	String index() {
+		return "index";
+	}
+
+}
