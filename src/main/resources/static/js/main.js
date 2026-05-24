807403c42f62d1ae9f72de508a171b9e06806f87
hjkikyou <2507110310X@gmail.com>
2026-05-25 00:44:15 +0900

    html+css+js 프론트엔드 끝

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,7 @@
+// src/main/resources/static/js/main.js 파일 내용
+
+const myButton = document.getElementById('hello-btn');
+
+myButton.addEventListener('click', function() {
+    alert('짜잔! 외부 자바스크립트 파일이 연결되었습니다! 🚀');
+});
