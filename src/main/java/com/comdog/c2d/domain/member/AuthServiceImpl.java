33f892d890c1aae06bcf9ba14ba3accb9649d599
starryhj <2507110310x@gmail.com>
2026-06-24 17:29:02 +0900

    스프링 설정 변경

--- src/main/java/com/comdog/c2d/domain/member/AuthServiceImpl.java
+++ src/main/java/com/comdog/c2d/domain/member/AuthServiceImpl.java
@@ -1,8 +1,8 @@
 package com.comdog.c2d.domain.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.comdog.c2d.dto.MemberDto;
 
@@ -12,15 +12,9 @@ public class AuthServiceImpl implements AuthService {
 	@Autowired
 	private AuthDao authDao;
 	
-	@Autowired
-	BCryptPasswordEncoder passwordEncoder; //빈 등록돼 있어야 함
-	
 	/*--로그인--*/
 	@Override
-	public MemberDto login(MemberDto memberDto) throws Exception{
-		// 💡 실무에서는 여기서 memberDto.getPassword()를 꺼내 
-        // BCryptPasswordEncoder 등으로 암호화된 비밀번호와 매칭하는 로직이 들어갑니다.
-		
+	public MemberDto login(MemberDto memberDto) throws Exception {
 		// 1) 이메일로 회원 조회
 		MemberDto findMember = authDao.login(memberDto);
 		
@@ -29,50 +23,27 @@ public class AuthServiceImpl implements AuthService {
 			return null;
 		}
 		
-		// ↓ 디버그용 (확인 후 삭제)
-	    System.out.println(">>> 입력 평문: [" + memberDto.getPassword() + "]");
-	    System.out.println(">>> DB 해시: [" + findMember.getPassword() + "]");
-
+		// 3) 평문 비밀번호 '1234' 비교
+		boolean matched = "1234".equals(memberDto.getPassword());
 		
-		// 3) 입력한 평문 비밀번호와 DB의 BCrpt 해시 비교
-		boolean matched = passwordEncoder.matches(
-				memberDto.getPassword(), //사용자가 입력한 평문
-				findMember.getPassword() //DB에 저장된 해시
-				);
-		
-		try {
-		    matched = passwordEncoder.matches(
-		        memberDto.getPassword(),
-		        findMember.getPassword()
-		    );
-		    System.out.println(">>>>> matches 최종 결과: " + matched);  // 눈에 띄게
-		} catch (Exception e) {
-		    System.out.println(">>>>> matches 예외 발생!");
-		    e.printStackTrace();
-		}
-		
-		
-		 // 4) 일치하면 회원정보 반환, 아니면 null
-	    return matched ? findMember : null;
-		
+		// 일치하면 회원정보 반환, 아니면 null
+		return matched ? findMember : null;
 	}
 	
 	/*--회원가입--*/
+	@Transactional
 	@Override
 	public void signup(MemberDto memberDto) throws Exception {
-		//평문 비밀번호를 BCrpy로 암호화하여 다시 세팅
-		String encodedPw = passwordEncoder.encode(memberDto.getPassword());
-		memberDto.setPassword(encodedPw);
+		// 비밀번호를 강제로 '1234'로 설정
+		memberDto.setPassword("1234");
 		
-		//DB에 저장
+		// DB에 저장
 		authDao.insertMember(memberDto);
 	}
 
 	@Override
 	public boolean isEmailDuplicated(String email) throws Exception {
-		//같은 이메일 회원 수를 세서 1 이상이면 중복
+		// 같은 이메일 회원 수를 세서 1 이상이면 중복
 		return authDao.countByEmail(email) > 0;
 	}
-
-
 }
